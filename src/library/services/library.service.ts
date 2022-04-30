e616e248c2df995d574cdef9fcb570548927345d
Vipan Kumar <vipan.webdeveloper@gmail.com>
2022-04-30 09:00:09 +0530

    init parsu mysql backend nodejs

--- src/library/services/library.service.ts
+++ src/library/services/library.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { Book } from "../entities/book.entity";
-
 @Injectable()
 export class LibraryService {
     constructor(
